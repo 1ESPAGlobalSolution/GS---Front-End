480285994af1eeb879d63294b849adb225455dbe
João Sarracine Faedrich <jsarracine@gmail.com>
2025-11-12 15:49:27 -0300

    Criando páginas das profissões e o login

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,21 @@
-function popUp(nome, info) {
+let paginaDestino = ""; // variável global para guardar o link
+
+function popUp(nome, info, pagina) {
   document.getElementById("nomeCandidato").textContent = nome;
   document.getElementById("infoCandidato").textContent = info;
   document.getElementById("popUp").style.display = "flex";
+
+  // guarda o link que o botão vai usar
+  paginaDestino = pagina;
 }
 
 function closePopUp() {
   document.getElementById("popUp").style.display = "none";
 }
+
+// quando clicar no botão "Ver mais", vai pra página
+document.getElementById("botao").addEventListener("click", function() {
+  if (paginaDestino) {
+    window.location.href = paginaDestino; // redireciona
+  }
+});
